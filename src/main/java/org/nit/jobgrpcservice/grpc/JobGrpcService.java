be67c1113b03e2a5030cf2ac96a38c1143aeb2d5
Naveen Prasanna <naveenprasannavnp@gmail.com>
2026-03-04 17:59:00 +0530

    added all other services

--- src/main/java/org/nit/jobgrpcservice/grpc/JobGrpcService.java
+++ src/main/java/org/nit/jobgrpcservice/grpc/JobGrpcService.java
@@ -2,35 +2,124 @@ package org.nit.jobgrpcservice.grpc;
 
 import io.grpc.stub.StreamObserver;
 import net.devh.boot.grpc.server.service.GrpcService;
-import org.nit.jobapp.JobRequest;
-import org.nit.jobapp.JobResponse;
-import org.nit.jobapp.JobServiceGrpc;
+import org.nit.jobgrpc.*;
 import org.nit.jobgrpcservice.model.JobPost;
 import org.nit.jobgrpcservice.service.JobService;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.List;
 
 @GrpcService
 public class JobGrpcService extends JobServiceGrpc.JobServiceImplBase {
 
-    private final JobService jobService;
-
-    public JobGrpcService(JobService jobService) {
-        this.jobService = jobService;
-    }
+    @Autowired
+    private JobService service;
 
     @Override
     public void getJob(JobRequest request,
                        StreamObserver<JobResponse> responseObserver) {
 
-        JobPost job = jobService.getJob(request.getPostId());
+        JobPost job = service.getJob(request.getPostId());
 
-        JobResponse response =
-                JobResponse.newBuilder()
-                        .setPostId(job.getPostId())
-                        .setProfile(job.getPostProfile())
-                        .setDescription(job.getPostDesc())
+        JobResponse response = mapToResponse(job);
+
+        responseObserver.onNext(response);
+        responseObserver.onCompleted();
+    }
+
+    @Override
+    public void getAllJobs(Empty request,
+                           StreamObserver<JobList> responseObserver) {
+
+        List<JobPost> jobs = service.getAllJobs();
+
+        List<JobResponse> responses =
+                jobs.stream().map(this::mapToResponse).toList();
+
+        JobList list = JobList.newBuilder()
+                .addAllJobs(responses)
+                .build();
+
+        responseObserver.onNext(list);
+        responseObserver.onCompleted();
+    }
+
+    @Override
+    public void addJob(JobResponse request,
+                       StreamObserver<JobResponse> responseObserver) {
+
+        JobPost job = mapToEntity(request);
+
+        service.addJob(job);
+
+        responseObserver.onNext(request);
+        responseObserver.onCompleted();
+    }
+
+    @Override
+    public void updateJob(JobResponse request,
+                          StreamObserver<JobResponse> responseObserver) {
+
+        JobPost job = mapToEntity(request);
+
+        service.updateJob(job);
+
+        responseObserver.onNext(request);
+        responseObserver.onCompleted();
+    }
+
+    @Override
+    public void deleteJob(JobRequest request,
+                          StreamObserver<DeleteResponse> responseObserver) {
+
+        service.deleteJob(request.getPostId());
+
+        DeleteResponse response =
+                DeleteResponse.newBuilder()
+                        .setMessage("Deleted")
                         .build();
 
         responseObserver.onNext(response);
         responseObserver.onCompleted();
+    }
+
+    @Override
+    public void searchJobs(SearchRequest request,
+                           StreamObserver<JobList> responseObserver) {
+
+        List<JobPost> jobs = service.search(request.getKeyword());
+
+        List<JobResponse> responses =
+                jobs.stream().map(this::mapToResponse).toList();
+
+        JobList list =
+                JobList.newBuilder()
+                        .addAllJobs(responses)
+                        .build();
+
+        responseObserver.onNext(list);
+        responseObserver.onCompleted();
+    }
+
+    private JobResponse mapToResponse(JobPost job) {
+
+        return JobResponse.newBuilder()
+                .setPostId(job.getPostId())
+                .setPostProfile(job.getPostProfile())
+                .setPostDesc(job.getPostDesc())
+                .setReqExperience(job.getReqExperience())
+                .addAllPostTechStack(job.getPostTechStack())
+                .build();
+    }
+
+    private JobPost mapToEntity(JobResponse res) {
+
+        return new JobPost(
+                res.getPostId(),
+                res.getPostProfile(),
+                res.getPostDesc(),
+                res.getReqExperience(),
+                res.getPostTechStackList()
+        );
     }
 }
